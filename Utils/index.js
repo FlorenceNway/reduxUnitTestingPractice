cee3ba8aae0e35d71b3404e3da9d4d0030d56295
Florence Nway <FlorenceNway@users.noreply.github.com>
2021-01-19 00:14:57 +0000

    seprate checkProp function for test

--- Utils/index.js
+++ Utils/index.js
@@ -1,4 +1,16 @@
+import checkPropTypes from "check-prop-types";
+
 export const findByTestAtrr = (component, attar) => {
   const wrapper = component.find(`[data-test='${attar}']`);
   return wrapper;
+};
+
+export const checkProps = (component, expectedProps) => {
+  const propsErr = checkPropTypes(
+    component.proptypes,
+    expectedProps,
+    "props",
+    component.name
+  );
+  return propsErr;
 };
