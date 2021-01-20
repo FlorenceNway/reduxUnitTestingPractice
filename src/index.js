622d5a018e0d74abeb05596e6a71f498fe99362d
Florence Nway <FlorenceNway@users.noreply.github.com>
2021-01-20 05:04:40 +0000

    store import as export

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
-import store from "./createStore";
+import { store } from "./createStore";
 import App from "./App";
 
 ReactDOM.render(
