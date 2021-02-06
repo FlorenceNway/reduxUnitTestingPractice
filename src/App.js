69e0479bcc6e70dafbdc852ccbc7be6b4f7d6428
Florence Nway <FlorenceNway@users.noreply.github.com>
2021-02-06 00:58:16 +0000

    shared button

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
 import React, { Component } from "react";
 import Header from "./components/header";
-import "./app.scss";
 import Headline from "./components/headline";
+import SharedButton from "./components/button/SharedButton";
+import ListItem from "./components/listitem/ListItem";
+import { connect } from "react-redux";
+import { fetchPosts } from "./actions";
+import "./app.scss";
 
 const tempArr = [
   {
@@ -12,7 +16,21 @@ const tempArr = [
 ];
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.fetch = this.fetch.bind(this);
+  }
+
+  fetch() {
+    this.props.fetchPosts();
+  }
+
   render() {
+    const { posts } = this.props;
+    const configButton = {
+      buttonText: "Get Posts",
+      emitEvent: this.fetch,
+    };
     return (
       <div>
         <Header />
@@ -21,9 +39,29 @@ class App extends Component {
           desc="Click the button to render posts"
           tempArr={tempArr}
         />
+        <SharedButton {...configButton} />
+        {posts.length > 0 && (
+          <div>
+            {posts.map((post, index) => {
+              const { title, body } = post;
+              const configListItem = {
+                title,
+                desc: body,
+              };
+              return <ListItem key={index} {...configListItem} />;
+            })}
+          </div>
+        )}
       </div>
     );
   }
 }
 
-export default App;
+const mapStateToProps = (state) => {
+  console.log(state);
+  return {
+    posts: state.postReducer,
+  };
+};
+
+export default connect(mapStateToProps, { fetchPosts })(App); //connect component to store
